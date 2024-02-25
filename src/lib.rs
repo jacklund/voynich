f7e7fa5394a4f681d77541863f07580895367d0a
Jack Lund <jackl@geekheads.net>
2024-02-25 13:59:21 -0600

    Fix doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,38 @@
 //! ## Example code
 //!
 //! ```no_run
+//! use anyhow::Result;
+//! use voynich::{
+//!     connect_to_tor,
+//!     create_onion_service,
+//!     Engine,
+//!     onion_service::OnionType,
+//!     logger::StandardLogger
+//! };
+//! use std::str::FromStr;
+//! use std::net::SocketAddr;
+//! use tor_client_lib::control_connection::{OnionServiceListener, TorSocketAddr};
+//!
+//! // My UI Application
+//! struct App {}
+//!
+//! impl App {
+//!    // Run the UI app
+//!    pub async fn run(
+//!        engine: &mut Engine,
+//!        listener: &OnionServiceListener,
+//!        logger: &mut StandardLogger,
+//!    ) -> Result<()> {
+//!      // Run your app
+//!      Ok(())
+//!    }
+//! }
+//!
 //! #[tokio::main]
 //! async fn main() -> Result<()> {
 //!     // Get a connection to Tor
 //!     let mut control_connection = connect_to_tor(
-//!         SocketAddr::from_str("127.0.0.1:9051"),
+//!         SocketAddr::from_str("127.0.0.1:9051").unwrap(),
 //!         None,
 //!         None,
 //!         None,
@@ -20,8 +47,8 @@
 //!     let (mut onion_service, onion_service_address, mut listener) = create_onion_service(
 //!         &mut control_connection,
 //!         OnionType::Transient,
-//!         3000,
-//!         TorSocketAddr::from_str("127.0.0.1:3000"),
+//!         Some(3000),
+//!         Some(TorSocketAddr::from_str("127.0.0.1:3000").unwrap()),
 //!     )
 //!     .await?;
 //!
@@ -29,7 +56,7 @@
 //!     let mut engine = Engine::new(
 //!         &mut onion_service,
 //!         onion_service_address,
-//!         SocketAddr::from_str("127.0.0.1:9050"),
+//!         SocketAddr::from_str("127.0.0.1:9050").unwrap(),
 //!         false,
 //!     )
 //!     .await?;
@@ -37,8 +64,8 @@
 //!     // Logging
 //!     let mut logger = StandardLogger::new(500);
 //!
-//!     // Start 'er up
-//!     let _ = App::run(&mut engine, &listener, &mut logger).await;
+//!     // Pass the engine, listener and logger to your application
+//!     App::run(&mut engine, &listener, &mut logger).await
 //! }
 //! ```
 
